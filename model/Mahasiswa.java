ad929e7841a5b31879f459b01daea6d4331d4359
ydhstr <yudhistiraekasatriaji@gmail.com>
2021-03-31 22:51:45 -0700

    Memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Mahasiswa{
+    public static void tampilanInfo(){
+        System.out.println();
+        System.out.println("==Data Mahasiswa=====================");
+        System.out.println("NPM : 19631128");
+        System.out.println("Nama : Yudhistira Eka Satriaji");
+        System.out.println("=====================================");
+
+    }
+}
